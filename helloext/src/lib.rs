80d95c4daf9a3434c447f5a917d9a369937b8af8
trevyn <230691+trevyn@users.noreply.github.com>
2023-10-16 20:36:18 +0300

    audio generator in rust

--- helloext/src/lib.rs
+++ helloext/src/lib.rs
@@ -1,4 +1,12 @@
+// cargo build --release --target aarch64-apple-darwin
+
 #![allow(unused)]
+use godot::engine::audio_stream_player;
+use godot::engine::AudioStreamGenerator;
+use godot::engine::AudioStreamGeneratorPlayback;
+use godot::engine::AudioStreamPlayback;
+use godot::engine::AudioStreamPlayer;
+use godot::engine::Label;
 use godot::engine::Sprite2D;
 use godot::engine::Sprite2DVirtual;
 use godot::prelude::*;
@@ -8,6 +16,73 @@ fn main() {
 
     #[gdextension]
     unsafe impl ExtensionLibrary for HelloWorld {}
+}
+
+#[derive(GodotClass)]
+#[class(base=Node)]
+struct Generator {
+    sample_hz: f32,
+    #[export]
+    pulse_hz: f32,
+    phase: f32,
+    playback: Option<Gd<AudioStreamGeneratorPlayback>>,
+
+    #[base]
+    base: Base<Node>,
+}
+
+#[godot_api]
+impl NodeVirtual for Generator {
+    fn init(base: Base<Node>) -> Self {
+        Self {
+            sample_hz: 44100.0,
+            pulse_hz: 440.0,
+            phase: 0.0,
+            playback: None,
+            base,
+        }
+    }
+
+    fn process(&mut self, delta: f64) {
+        self.fill_buffer()
+    }
+
+    fn ready(&mut self) {
+        let mut audio_stream_player = self
+            .base
+            .get_node_as::<AudioStreamPlayer>("AudioStreamPlayer");
+
+        let mut audio_stream_generator = audio_stream_player
+            .get_stream()
+            .unwrap()
+            .cast::<AudioStreamGenerator>();
+
+        audio_stream_generator.set_mix_rate(self.sample_hz);
+        audio_stream_generator.set_buffer_length(0.024);
+        audio_stream_player.play();
+        self.playback = Some(audio_stream_player.get_stream_playback().unwrap().cast());
+        self.fill_buffer();
+    }
+}
+
+#[godot_api]
+impl Generator {
+    fn fill_buffer(&mut self) {
+        let increment = self.pulse_hz / self.sample_hz;
+
+        let playback = self.playback.as_mut().unwrap();
+
+        let to_fill = playback.get_frames_available();
+        for i in 0..to_fill {
+            let v = Vector2::ONE * (self.phase * std::f32::consts::TAU).sin();
+            self.phase = (self.phase + increment) % 1.0;
+            playback.push_frame(v);
+        }
+
+        self.base
+            .get_node_as::<Label>("Label")
+            .set_text(format!("Skips = {}", playback.get_skips()).into());
+    }
 }
 
 #[derive(GodotClass)]
@@ -23,7 +98,7 @@ struct Player {
 #[godot_api]
 impl Sprite2DVirtual for Player {
     fn init(sprite: Base<Sprite2D>) -> Self {
-        godot_print!("Hello world from Rust!"); // Prints to the Godot console
+        godot_print!("Hello world from Rust!");
 
         Self {
             speed: 400.0,
