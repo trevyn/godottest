41fdd59d0ed146421b66efda04f0dda2c18a5305
trevyn <230691+trevyn@users.noreply.github.com>
2023-10-16 11:26:47 +0300

    knob

--- Area2D.gd
+++ Area2D.gd
@@ -1,11 +1,7 @@
 extends Area2D
 
-func _input_event(viewport, event, shape_idx):
+func _input_event(_viewport, event, _shape_idx):
 	if event is InputEventMouseButton \
-	and event.button_index == MOUSE_BUTTON_LEFT \
-	and event.is_pressed():
-		self.on_click()
-
-func on_click():
-	print("Click")
-	$"../Generator".do_thing()
+	and event.button_index == MOUSE_BUTTON_LEFT:
+		if event.is_pressed():
+			$"../Generator".do_thing()
---
+++ knob.gd
@@ -0,0 +1,25 @@
+extends Node2D
+
+#signal turnedKnob
+
+var following := false
+const MAX_DIST := 7000
+
+func _physics_process(delta: float) -> void:
+	var mouseDist := get_global_mouse_position().distance_squared_to(global_position)
+	if mouseDist < MAX_DIST and Input.is_action_just_pressed("click"):
+		following = true
+	if Input.is_action_just_released("click"):
+		following = false
+		
+	if following:
+		var ang := get_global_mouse_position().angle_to_point(global_position) + PI/2
+		rotation = ang
+		$"../Generator".pulse_hz = 440+(ang*40)
+#		var d :Vector2= ($knob/knobPoint.position.rotated(.rotation))
+#		var a = $middlePoint.position.angle_to(d)
+#		var finalAng :float= range_lerp( a, -3.14, 3.14, 0, 100 )
+#		print(finalAng)
+#		var fang :float= lerp_angle( $knob.rotation, ang, 0.3  )
+#		$knob.rotation = clamp(fang, -2,2)
+#		emit_signal("turnedKnob", finalAng)
